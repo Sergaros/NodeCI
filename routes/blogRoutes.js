3b513dca9611c1b77049406c1b1b961092531ae8
Sergej <sergej.tertychnij@gmail.com>
2018-09-10 17:13:00 +0300

    changes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanHash = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
+
+// const redis = require('redis');
+// const redisUrl = 'redis://127.0.0.1:6379';
+// const client = redis.createClient(redisUrl);
+// const util = require('util');
+// client.get = util.promisify(client.get);
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -14,12 +21,14 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const blogs = await Blog
+        .find({ _user: req.user.id })
+        .cache({key: req.user.id});
 
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanHash, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
